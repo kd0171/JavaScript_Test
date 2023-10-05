6b68bbb8ffb0d48a8a0e02e53423ae326c8df973
kd0171 <kido.0117.1895@gmail.com>
2023-10-05 06:46:09 +0200

    L11

--- main4.js
+++ main4.js
@@ -117,4 +117,16 @@ if(level>10){
     console.log("レベルが10より大きいです");
 }
         
+const age = 24;
+
+// 「age >= 20」を出力してください
+console.log(age>=20);
+
+// 「age < 20」を出力してください
+console.log(age<20);
+
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if(age>=20){
+  console.log("私は20歳以上です");
+}
 
