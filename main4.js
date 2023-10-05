7af15731826c2ff26761d6dedc233fd279c78a56
kd0171 <kido.0117.1895@gmail.com>
2023-10-05 06:51:23 +0200

    L12

--- main4.js
+++ main4.js
@@ -130,3 +130,51 @@ if(age>=20){
   console.log("私は20歳以上です");
 }
 
+        /*
+            比較演算子には、左と右の値が等しいかを調べるものもあります。
+            「a === b」はaとbが等しければtrue、等しくなければfalseになります。
+            「a !== b」はその逆です。
+        */
+
+const password = "ninjawanko";
+
+// passwordの値が"ninjawanko"の場合、「ログインに成功しました」と出力してください
+if(password === "ninjawanko"){
+    console.log("ログインに成功しました");
+}
+
+
+
+// passwordの値が"ninjawanko"でない場合、「パスワードが間違っています」と出力してください
+if(password !== "ninjawanko"){
+    console.log("パスワードが間違っています");
+}
+
+const age = 17;
+
+// 条件式が成り立たない場合に「私は20歳未満です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else{
+  console.log("私は20歳未満です");
+}
+
+const age = 17;
+
+// ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else if(age>=10){
+  console.log("私は20歳未満ですが、10歳以上です");
+}
+
+ else {
+  console.log("私は10歳未満です");
+}
+
+
+
+
+            
+
+
