3ce0a7fe3ca0b6042f65353d7bb5073b3ab0840f
kd0171 <kido.0117.1895@gmail.com>
2023-10-06 04:59:58 +0200

    L4

--- main5.js
+++ main5.js
@@ -60,11 +60,32 @@ console.log(animals[0]);
 console.log(animals[2]);
 
 
+const animals = ["dog", "cat", "sheep"];
+
+// 配列animalsの3つ目の要素を「rabbit」に更新してください
+animals[2]="rabbit";
+
+// 配列animalsの3つ目の要素をコンソールに表示して下さい
+console.log(animals[2]);
 
 
+const animals = ["dog", "cat", "sheep"];
+
+// for文を用いて、配列の値を順にコンソールに出力してください
+for(let i =0;i<3;i++){
+  console.log(animals[i]);
+}
 
 
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
 
+// lengthを用いて配列の要素の数を出力してください
+console.log(animals.length);
+
+// lengthを用いて条件式を書き換えてください
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
 
 
 
