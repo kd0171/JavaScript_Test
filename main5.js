73b57d4e1b54c2ea6f84a8487c18f198b567f801
kd0171 <kido.0117.1895@gmail.com>
2023-10-06 05:04:39 +0200

    L7

--- main5.js
+++ main5.js
@@ -94,3 +94,16 @@ const character ={name:"にんじゃわんこ",age:14};
 console.log(character);
 
 
+const character = {name: "にんじゃわんこ", age: 14};
+
+// characterのnameの値を出力してください
+console.log(character.name);
+
+// characterのageの値を「20」に更新してください
+character.age = 20;
+
+// characterをコンソールに出力してください
+console.log(character);
+
+
+
