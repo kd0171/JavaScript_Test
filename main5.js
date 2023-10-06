a256fbe0ecb58e71fac4cc25bd77892a3e9baa4a
kd0171 <kido.0117.1895@gmail.com>
2023-10-06 04:17:40 +0200

    L2

--- main5.js
+++ main5.js
@@ -17,3 +17,24 @@ number += 1;
 console.log(number);
 
 
+// 変数numberを定義してください
+let number = 1;
+
+// while文を作成してください
+while(number<=100){
+  console.log(number);
+  number += 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
