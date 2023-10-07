ea0f6d069a3af8e14137315e6accf39d127b34e7
kd0171 <kido.0117.1895@gmail.com>
2023-10-07 17:22:04 +0200

    L1

---
+++ main7.js
@@ -0,0 +1,72 @@
+// 定数animalを定義してください
+const animal = {
+    name:"レオ",
+    age:3,
+    greet:() => {
+      console.log("こんにちは");
+    }
+  }
+  
+  // animalのnameプロパティの値を出力してください
+  console.log(animal.name);
+  
+  // animalのgreetプロパティの関数を実行してください
+  animal.greet();
+  
+  // Animalクラスを定義してください
+class Animal{
+  
+}
+
+class Animal {
+}
+
+// Animalクラスのインスタンスを定数animalに代入してください
+const animal = new Animal();
+
+// 定数animalの値を出力してください
+console.log(animal);
+
+
+class Animal {
+    // コンストラクタを追加してください
+    constructor(){
+      console.log("インスタンスを生成しました");
+    }
+    
+  }
+  
+
+  
+  const animal = new Animal();
+  
+  class Animal {
+    constructor() {
+      // nameの値に文字列「レオ」を代入してください
+      this.name = "レオ";
+      
+      // ageの値に数値の「3」を代入してください
+      this.age = 3;
+    }
+  }
+  
+  const animal = new Animal();
+  
+  // 「名前: 〇〇」となるように出力してください
+  console.log(`名前：${animal.name}`);
+  
+  // 「年齢: 〇〇」となるように出力してください
+  console.log(`年齢：${animal.age}`);
+  
+  
+
+
+
+
+
+
+
+
+
+
+
