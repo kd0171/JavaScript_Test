82849b91383a1f174ec3fc2e52d37fdc7b84ff34
kd0171 <kido.0117.1895@gmail.com>
2023-10-07 16:43:31 +0200

    L5

--- main6.js
+++ main6.js
@@ -61,6 +61,71 @@ const add = (number1,number2) => {
   console.log(`130の半分は${result}です`);
   
 
+  const check = (number) => {
+    // numberが3の倍数かどうかを戻り値として返してください
+    return number%3===0;
+            //真偽値が戻り値
+  };
+  
+  // if文の条件式で、checkを呼び出してください
+  if (check(123)) {
+    console.log("3の倍数です");
+  } else {
+    console.log("3の倍数ではありません");
+  }
+  
+
+  /*
+  関数の内側と外側で同じ名前の変数を定義した場合は、それらは別々のものとして扱われます。
+  下の図では、introduce関数の内側と外側で同じ名前の定数nameを定義していますが、これらは別のものとして扱われます。
+  
+  */
+
+
+
+// 関数の外側に定数nameを定義してください
+const name = "ひつじ仙人";
+
+const introduce = () => {
+  // 関数の内側に定数nameを定義してください
+  const name = "にんじゃわんこ";
+
+
+  // 定数nameを出力してください
+  console.log(name);
+  
+};
+
+introduce();
+
+// コードを貼り付けて、定数nameを出力してください。
+console.log(name);
+
+
+// toMinutes関数を定義してください
+const toMinutes = (hour,minute)=>{
+    return hour*60+minute;
+  } 
+  
+  // 定数resultに、toMinutes関数の戻り値を代入してください
+  const result =  toMinutes(3,20);
+  
+  // 「◯◯分」となるように、分に換算した結果を出力してください
+  console.log(`${result}分`);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
