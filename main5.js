4549b11a3731038b70ff0a7e774db1369dc79ad1
kd0171 <kido.0117.1895@gmail.com>
2023-10-07 15:49:08 +0200

    L11

--- main5.js
+++ main5.js
@@ -184,3 +184,40 @@ const characters = [
     }
   }
   
+  const cafe = {
+    name: "Progateカフェ",
+    businessHours: {
+      // businessHoursの値に指定されたオブジェクトを代入してください
+      opening: "10:00(AM)",
+      closing: "8:00(PM)"
+    }
+  };
+  
+  // 「店名:〇〇」を出力してください
+  console.log(`店名:${cafe.name}`);
+  
+  // 「営業時間:〇〇から△△」を出力してください
+  console.log(`営業時間: ${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+
+
+  const cafe = {
+    name: "Progateカフェ",
+    businessHours: { 
+      opening: "10:00(AM)",
+      closing: "8:00(PM)"
+    },
+    // menusプロパティに配列を代入してください
+    menus:["コーヒー","紅茶","チョコレートケーキ"]
+  };
+            //配列でも代入可能
+  
+  console.log(`店名: ${cafe.name}`);
+  console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+  console.log(`----------------------------`);
+  console.log("おすすめメニューはこちら");
+  
+  // for文を用いて配列menusの中身を表示させてください
+  for(let i = 0; i<cafe.menus.length;i++){
+    console.log(cafe.menus[i]);
+  }
+  
