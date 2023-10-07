61acc5081cdb6309c854854c97ac9236b991c549
kd0171 <kido.0117.1895@gmail.com>
2023-10-08 01:56:30 +0200

    L4

--- main7.js
+++ main7.js
@@ -155,5 +155,85 @@ class Animal {
   animal.info();
   
 
+  class Animal {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    greet() {
+      console.log("こんにちは");
+    }
+    
+    info() {
+      this.greet();
+      console.log(`名前は${this.name}です`);
+      console.log(`${this.age}歳です`);
+    }
+  }
+  
+  class Dog extends Animal {
+  }
+  
+  // 定数dogにDogクラスのインスタンスを代入してください
+  const dog = new Dog("レオ",4);
+  
+  // dogに対してinfoメソッドを呼び出してください
+  dog.info();
+  
+
+  class Animal {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    greet() {
+      console.log("こんにちは");
+    }
+    
+    info() {
+      this.greet();
+      console.log(`名前は${this.name}です`);
+      console.log(`${this.age}歳です`);
+    }
+  }
+  
+  class Dog extends Animal {
+    // getHumanAgeメソッドを追加してください
+    getHumanAge(){
+      return this.age*7;
+    }
+  
+  }
+  
+  const dog = new Dog("レオ", 4);
+  dog.info();
+  
+  // 定数humanAgeを定義し、定数dogに対してgetHumanAgeメソッドを呼び出した値を代入してください
+  const humanAge =dog.getHumanAge();
+  
+  // 「人間年齢で〇〇歳です」と出力してください
+  console.log(`人間年齢で${humanAge}歳です`);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
