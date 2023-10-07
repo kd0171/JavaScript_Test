6184b7a71b16cc7e8a8688ba37b253f95b7aede3
kd0171 <kido.0117.1895@gmail.com>
2023-10-07 15:59:14 +0200

    L2

--- main6.js
+++ main6.js
@@ -6,3 +6,15 @@ const greet = function() {
   // 関数を呼び出してください
   greet();
   
+
+
+  // 定数helloに関数を代入してください
+const hello = function() {
+    console.log("こんにちは！");
+    console.log("私の名前は○○です");
+  }
+  
+  
+  // 定数helloに代入された関数を呼び出してください
+  hello();
+  
