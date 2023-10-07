39f98f98295f4553e9aa9eb6df44ae4fa75926c6
kd0171 <kido.0117.1895@gmail.com>
2023-10-08 01:27:02 +0200

    L2

--- main7.js
+++ main7.js
@@ -37,7 +37,7 @@ class Animal {
   }
   
 
-  
+
   const animal = new Animal();
   
   class Animal {
@@ -63,6 +63,48 @@ class Animal {
 
 
 
+        //コンストラクターの書き換え可能にするため
+class Animal {
+  // 引数に「name」と「age」を追加してください
+  constructor(name,age) {
+    // 「"レオ"」の代わりに引数nameの値を代入してください
+    this.name = name;
+    
+    // 「3」の代わりに引数ageの値を代入してください
+    this.age = age;
+  }
+}
+
+// 引数に「"モカ"」と「8」を渡してください
+const animal = new Animal("モカ",8);
+
+console.log(`名前: ${animal.name}`);
+console.log(`年齢: ${animal.age}`);
+
+
+
+class Animal {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    // greetメソッドを追加してください
+    greet(){
+      console.log("こんにちは");
+    }
+        //クラス内のメソッドは関数式とは違う書き方？
+  }
+  
+  const animal = new Animal("レオ", 3);
+  
+  console.log(`名前: ${animal.name}`);
+  console.log(`年齢: ${animal.age}`);
+  
+  // animalに対してgreetメソッドを呼び出してください
+  animal.greet();
+
+  
 
 
 
