a487326289a4e6e5a35b8c908d61b86331e21469
kd0171 <kido.0117.1895@gmail.com>
2023-10-09 04:10:07 +0200

    L3

--- SQL1.sql
+++ SQL1.sql
@@ -47,7 +47,20 @@ where purchased_at <= "2017-08-01";
 
 
 
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」を含まないデータを取得してください
 
+SELECT *
+FROM purchases
+WHERE NOT name LIKE "%プリン%";
 
+-- 「FROM purchases」のあとにコードを追加し、priceカラムがNULLであるデータを取得してください
 
+SELECT *
+FROM purchases
+Where price is null;
 
+-- 「FROM purchases」のあとにコードを追加し、priceカラムがNULLでないデータを取得してください
+
+SELECT *
+FROM purchases
+where price is not null;
