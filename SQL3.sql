b8384884e602fa04dda5abfa098b151202610207
kd0171 <kido.0117.1895@gmail.com>
2023-10-10 05:57:17 +0200

    L1

---
+++ SQL3.sql
@@ -0,0 +1,60 @@
+SELECT goals
+FROM players
+WHERE name="ウィル"
+;
+
+SELECT *
+FROM players
+WHERE goals > 14
+;
+
+/*
+( )で囲むことで、サブクエリを使用できます。サブクエリの書き方は基本的に通常のクエリと同じですが、()内にセミコロン（;）は不要です。
+*/
+
+SELECT name
+FROM players
+WHERE goals > (
+  -- この下にウィルの得点数を取得するクエリを書いてください
+  SELECT goals
+  FROM players
+  WHERE name = "ウィル"
+)
+;
+
+SELECT name,goals
+FROM players
+WHERE goals > (
+  SELECT AVG(goals)
+  FROM players
+)
+;
+
+
+--ASを使って、取得したカラム名を"身長180cm以上の選手"としてください
+SELECT name AS "身長180cm以上の選手"
+FROM players
+WHERE height >= 180
+;
+
+
+--ASを使って、取得したカラム名を"チームの合計得点"としてください
+SELECT SUM(goals) AS "チームの合計得点"
+FROM players
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
