a36a514317299d491c151787b083d616cf1a7146
kd0171 <kido.0117.1895@gmail.com>
2023-10-11 04:24:30 +0200

    L1

---
+++ SQL4.sql
@@ -0,0 +1,24 @@
+    /*
+    多くの場合、idカラムにはAUTO INCREMENTという機能が使われます。
+    AUTO INCREMENTとは「自動で増加する」といった意味で、データが作成されるときに自動で値が割り当てられます。
+    */
+
+-- studentsテーブルにデータを追加してください。
+INSERT INTO students(name, course)
+VALUES("Kate","Java")
+;
+-- 下記のクエリは消さないでください。
+select * from students
+;
+
+    /*
+    データを更新したいときにはUPDATEを使います。
+    複数変更したいカラムがある場合、コンマ(,)で区切ります。
+    s*/
+
+
+
+
+
+
+
